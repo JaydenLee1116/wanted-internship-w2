84eb822de23715ec755e5f271d0cec4f6a086d6d
JaydenLee1116 <vv55adss@gmail.com>
2023-08-30 22:28:10 +0900

    ✨ feat: useIssueNumberLocation 커스텀 훅 구현
    - react-router-dom의 useLocation을 사용하여 해당 issue number를 검증하는 훅

---
+++ src/hooks/useGetIssue.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { AxiosError } from 'axios';
+
+import { axiosFetch } from '../api/axiosInstance';
+
+import { GITHUB_API_PATH } from '../api/apiConfig';
+import { Issue } from '../types';
+import { useRepoNameStore, useRepoOwnerNameStore } from './useStore';
+
+interface useGetIssuesProps {
+  issueNumber: number;
+}
+
+export const useGetIssue = ({ issueNumber }: useGetIssuesProps) => {
+  const [data, setData] = useState<Issue | null>(null);
+  const [error, setError] = useState<AxiosError | null>(null);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { repoName } = useRepoNameStore();
+  const { repoOwnerName } = useRepoOwnerNameStore();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+      setIsError(false);
+      try {
+        const response = await axiosFetch.get(
+          GITHUB_API_PATH.getIssue(repoOwnerName, repoName, issueNumber),
+        );
+        const { data } = response;
+        setData(data);
+      } catch (error) {
+        setError(error as AxiosError);
+        setIsError(true);
+      }
+    };
+    fetchData();
+    setIsLoading(false);
+  }, []);
+
+  return {
+    data,
+    error,
+    isError,
+    isLoading,
+  };
+};
