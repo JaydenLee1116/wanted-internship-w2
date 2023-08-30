840867930870f3a0544919cb9efcf1171efc5a8d
JaydenLee1116 <vv55adss@gmail.com>
2023-08-30 22:34:44 +0900

    ✨ feat: getIssue 함수 추가

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -3,4 +3,6 @@ export const BASE_GITHUB_API_URL = 'https://api.github.com/repos';
 export const GITHUB_API_PATH = Object.freeze({
   getIssues: (owner: string, repo: string, page: number) =>
     `${BASE_GITHUB_API_URL}/${owner}/${repo}/issues?per_page=10&page=${page}`,
+  getIssue: (owner: string, repo: string, issueNumber: number) =>
+    `${BASE_GITHUB_API_URL}/${owner}/${repo}/issues/${issueNumber}`,
 });
