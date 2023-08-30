20ece623ab757d49188ea4bc174732ca3f11a844
JaydenLee1116 <vv55adss@gmail.com>
2023-08-30 22:28:10 +0900

    ✨ feat: useGetIssuen 커스텀 훅 구현
    - 특정 issue에 대한 값을 가져오는 훅

---
+++ src/hooks/useIssueNumberLocation.ts
@@ -0,0 +1,8 @@
+import { useLocation } from 'react-router-dom';
+
+export const useIssueNumberLocation = () => {
+  const { pathname } = useLocation();
+  const issueNumber = Number(pathname.split('/').pop());
+  if (isNaN(issueNumber)) throw new Error('Issue number is not a number');
+  return { issueNumber };
+};
