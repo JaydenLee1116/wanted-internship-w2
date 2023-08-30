bcb1df91191b9caceba87cccb43cb16547872c59
JaydenLee1116 <vv55adss@gmail.com>
2023-08-30 22:33:50 +0900

    ✨ feat: useIssueNumberLocation 커스텀 훅 구현
    - useLocation을 통해 issue number를 가져오고 검증하는 훅

--- src/hooks/useIssueNumberLocation.ts
+++ src/hooks/useIssueNumberLocation.ts
@@ -4,5 +4,6 @@ export const useIssueNumberLocation = () => {
   const { pathname } = useLocation();
   const issueNumber = Number(pathname.split('/').pop());
   if (isNaN(issueNumber)) throw new Error('Issue number is not a number');
+
   return { issueNumber };
 };
