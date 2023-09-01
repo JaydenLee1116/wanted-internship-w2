f4f5ed31e029f9ff26e7cae35f16a7bc7112251a
JaydenLee1116 <vv55adss@gmail.com>
2023-09-01 22:11:24 +0900

    ♻️ refactor: IssueItem 내부로 Link 이동

--- src/components/IssueList/IssueItem/index.tsx
+++ src/components/IssueList/IssueItem/index.tsx
@@ -15,13 +15,15 @@ interface IssueItemProps {
 export const IssueItem = ({ number, title, login, created_at, comments }: IssueItemProps) => {
   return (
     <S.Container>
-      <S.Left>
-        <S.MainInfo>{`#${number} ${title}`}</S.MainInfo>
-        <S.SubInfo>{`작성자: ${login}, 작성일: ${getKoreanDate(created_at)}`}</S.SubInfo>
-      </S.Left>
-      <S.Right>
-        <S.Comment>{`코멘트: ${comments}`}</S.Comment>
-      </S.Right>
+      <S._Link to={`/detail/${number}`}>
+        <S.Left>
+          <S.MainInfo>{`#${number} ${title}`}</S.MainInfo>
+          <S.SubInfo>{`작성자: ${login}, 작성일: ${getKoreanDate(created_at)}`}</S.SubInfo>
+        </S.Left>
+        <S.Right>
+          <S.Comment>{`코멘트: ${comments}`}</S.Comment>
+        </S.Right>
+      </S._Link>
     </S.Container>
   );
 };
